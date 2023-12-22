c5a6aaef3f5240e94e12dd597be0c4be4f6fd89d
root <root@hp-home>
2023-12-22 09:51:00 +0000

    m

--- CLI_helper.py
+++ CLI_helper.py
@@ -2,27 +2,44 @@
 from HiLinkAPI import webui
 
 import logging
+import sys
 from time import sleep, time
 from datetime import datetime
 
 logging.basicConfig(filename="hilinkapitest.log", format='%(asctime)s --  %(message)s', level=logging.DEBUG, datefmt="%Y-%m-%d %I:%M:%S %p:%Z")
 
-
-webUI=webui("E", "192.168.10.14", "admin",  'Labuga21!!!!'  , logger=logging, scheme='https')
+webUI=webui("mymodem", "192.168.10.18", "admin",  'Labuga21!!!!'  , logger=logging, scheme='https')
 
 webUI.start()
 
-while not webUI.getValidSession():
-                # check for active errors
-                if webUI.getActiveError() is not None:
-                    error = webUI.getActiveError()
-                    print(error)
-                    sleep(5)
-                # check for login wait time
-                if webUI.getLoginWaitTime() > 0:
-                    print(f"Login wait time available = {webUI.getLoginWaitTime()} minutes")
-                    sleep(5)
+max_attempts=5
+i=0
 
+while not webUI.getValidSession():
+    sleep(1)
+    i+=1
+    print(f"= logging in, attempt {i}/{max_attempts}")
+    # check for active errors
+    if webUI.getActiveError() is not None:
+           error = webUI.getActiveError()
+           print(f"= {error}")
+           sleep(1)
+    else:
+        pass
+    # check for login wait time
+    if webUI.getLoginWaitTime() > 0:
+           print(f"= Login wait time available = {webUI.getLoginWaitTime()} minutes")
+           sleep(1)
+    else:
+        pass
+    if i  == max_attempts :
+        print(f"= max attempts reached")
+        webUI.stop()
+        while(not webUI.isStopped()):
+             webUI.stop()
+             print(f"= Waiting for stop")
+             sleep(1)
+        sys.exit(1)
 
 print("= got session")
 
@@ -44,6 +61,6 @@ if True:
 webUI.stop()
 while(not webUI.isStopped()):
      webUI.stop()
-     print(f"Waiting for stop")
+     print(f"= Waiting for stop")
      sleep(1)
 
